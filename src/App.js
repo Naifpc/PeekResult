25db7a1aad2c8b67a606a5e49809394a0ff52b1f
Naifpc <naif--556677@outlook.sa>
2025-02-03 20:51:53 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/partials/modals.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,59 @@
-import './App.css';
-import Home from './pages/home';
-import Schedules from './pages/schedules';
-import Offers from './pages/offers';
-import Login from './pages/login';
-import Account from './pages/account';
-import Navbar from './partials/navbar';
-import Footer from './partials/footer';
-import AccountData from './pages/accountData';
-import MySubscription from './pages/mySubscription';
-import AboutTrainer from './pages/aboutTrainer';
-import LoginModal from "./partials/modals.js"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Home from "./pages/home";
+import Schedules from "./pages/schedules";
+import Offers from "./pages/offers";
+import Account from "./pages/account";
+import Navbar from "./partials/navbar";
+import Footer from "./partials/footer";
+import AccountData from "./pages/accountData";
+import MySubscription from "./pages/mySubscription";
+import AboutTrainer from "./pages/aboutTrainer";
+import { login as Login, register as Register } from "./partials/modals.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar/>
+      <>
+        <BrowserView>
+          <Navbar />
 
-        <Routes>
-          <Route path='/Account' element={<Account />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/Schedules' element={<Schedules />} />
-          <Route path='/Offers' element={<Offers />} />
-          <Route path='/Login' element={<Login />} />
-          <Route path='/accountData' element={<AccountData />} />
-          <Route path='/mySubscription' element={<MySubscription />} />
-          <Route path='/aboutTrainer' element={<AboutTrainer />} />
-        </Routes>
-        
-        <Footer/>
-        <LoginModal/>
-      </div>
+          <Routes>
+            <Route path="/Account" element={<Account />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/Schedules" element={<Schedules />} />
+            <Route path="/Offers" element={<Offers />} />
+            <Route path="/accountData" element={<AccountData />} />
+            <Route path="/mySubscription" element={<MySubscription />} />
+            <Route path="/aboutTrainer" element={<AboutTrainer />} />
+          </Routes>
+
+          <Footer />
+        </BrowserView>
+
+        <MobileView>
+          <Routes>
+            <Route path="/Account" element={<Account />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/Schedules" element={<Schedules />} />
+            <Route path="/Offers" element={<Offers />} />
+            <Route path="/accountData" element={<AccountData />} />
+            <Route path="/mySubscription" element={<MySubscription />} />
+            <Route path="/aboutTrainer" element={<AboutTrainer />} />
+          </Routes>
+        </MobileView>
+
+        <Login />
+        <Register />
+      </>
     </Router>
   );
 }
 
 export default App;
-
-
-
-
--- src/partials/modals.js
+++ src/partials/modals.js
@@ -12,78 +12,82 @@ function login() {
     >
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content border-0  p-5">
-        <form>
-          <div class="modal-headerborder-0">
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <h4 className="fw-bold">تسجيل الدخول</h4>
-            <h6 className="text-secondary">
-              عند تسجيل دخولك فانت فانت توافق على 
-              
-              <Link className="link-offset-2  link-offset-3-hover
-                                link-underline link-underline-opacity-0 
-                                link-underline-opacity-75-hover"> سياسة الاستخدام </Link> 
-                                
-                                  و اطلعت على
-              
-              
-              <Link className="link-offset-2  link-offset-3-hover
-                                link-underline link-underline-opacity-0 
-                                link-underline-opacity-75-hover"> سياسة الخصوصية </Link>
-            </h6 >
-              <div className="my-4">
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">
-                  الايميل
-                </label>
-                <input
-                  type="email"
-                  class="form-control bg-body-secondary border-0 shadow"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  placeholder="example@email.com"
-                />
-
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">
-                  كلمة السر
-                </label>
-                <input
-                  type="password"
-                  class="form-control  bg-body-secondary border-0 shadow"
-                  id="exampleInputPassword1"
-                />
-              </div>
-              <div className="mt-4 px-5 d-flex justify-content-center">
-              <button type="button" class="btn btn-secondary w-75   ">
-              تسجيل الدخول
-            </button>
-
-              </div>
-              
-
-              
-              </div>
-              
-             
-             
-            
-          </div>
-          <div class="modal-footer justify-content-center  ">
-          <p className="fs-6 ">ليس لديك حساب؟  
-                 <Link className="link-offset-2  link-offset-3-hover
+          <form>
+            <div class="modal-headerborder-0">
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <h4 className="fw-bold">تسجيل الدخول</h4>
+              <h6 className="text-secondary">
+                عند تسجيل دخولك فانت فانت توافق على
+                <Link
+                  className="link-offset-2  link-offset-3-hover
                                 link-underline link-underline-opacity-0 
                                 link-underline-opacity-75-hover"
-                 > تسجيل حساب  </Link></p>
-            
-          </div>
+                >
+                  {" "}
+                  سياسة الاستخدام{" "}
+                </Link>
+                و اطلعت على
+                <Link
+                  className="link-offset-2  link-offset-3-hover
+                                link-underline link-underline-opacity-0 
+                                link-underline-opacity-75-hover"
+                >
+                  {" "}
+                  سياسة الخصوصية{" "}
+                </Link>
+              </h6>
+              <div className="my-4">
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">
+                    الايميل
+                  </label>
+                  <input
+                    type="email"
+                    class="form-control bg-body-secondary border-0 shadow"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="example@email.com"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">
+                    كلمة السر
+                  </label>
+                  <input
+                    type="password"
+                    class="form-control  bg-body-secondary border-0 shadow"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+                <div className="mt-4 px-5 d-flex justify-content-center">
+                  <button type="button" class="btn btn-secondary w-75   ">
+                    تسجيل الدخول
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer justify-content-center  ">
+              <p className="fs-6 ">
+                ليس لديك حساب؟
+                <button
+                  type="button"
+                  className="btn btn-link link-offset-2  link-offset-3-hover
+                                link-underline link-underline-opacity-0 
+                                link-underline-opacity-75-hover pt-0"
+                  data-bs-toggle="modal"
+                  data-bs-target="#registerModal"
+                >
+                  تسجيل حساب
+                </button>
+              </p>
+            </div>
           </form>
         </div>
       </div>
@@ -91,4 +95,31 @@ function login() {
   );
 }
 
-export default login;
+function register() {
+  return (
+    <div
+      class="modal fade"
+      id="registerModal"
+      tabindex="-1"
+      aria-labelledby="registerModal"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content">
+          <div class="modal-header border-0">
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body  ">...</div>
+          <div class="modal-footer border-0"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { login, register };
